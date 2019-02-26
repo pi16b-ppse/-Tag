b98467323f5fa09bff2fd6502dc2a3b408f34a23
Nikita <36260669+magus98@users.noreply.github.com>
2019-02-26 22:05:12 +0300

    Update function

--- 15.js
+++ 15.js
@@ -1,3 +1,13 @@
 function newGame(){
+var arr = [];
+for(i = 0; i &lt; 4; ++i){
+		arr[i] = [];
+		for(j = 0; j &lt; 4; ++j){
+			if(i + j != 6)
+				arr[i][j] = i*4 + j + 1;
+			else
+				arr[i][j] = "";
+		}
+	}
 
 }
