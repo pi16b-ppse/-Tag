e2daa22a03d0b68feeaa5e95c0ab4b2cea22b9a6
Nikita <36260669+magus98@users.noreply.github.com>
2019-05-19 21:11:44 +0300

    Загрузка полного проекта

--- 15.js
+++ 15.js
@@ -1,69 +1,68 @@
-﻿function newGame(){
-var arr = [];
-for(i = 0; i &lt; 4; ++i){
-		arr[i] = [];
-		for(j = 0; j &lt; 4; ++j){
+var arr = [], box, ei,ej;						
+function swap(arr,i1,j1,i2,j2){				
+	t = arr[i1][j1];
+	arr[i1][j1] = arr[i2][j2];
+	arr[i2][j2] = t;
+}
+window.onload = function() {				
+	box = document.getElementById("box");
+	newGame();				
+	document.getElementById("reset").onclick = newGame;						
+}
+function cellClick(event) {
+	var event = event || window.event,
+		el = event.srcElement || event.target,
+		i = el.id.charAt(0),
+		j = el.id.charAt(2);
+	if((i == ei && Math.abs(j - ej) == 1) || (j == ej && Math.abs(i - ei) == 1)){					
+		document.getElementById(ei + " " + ej).innerHTML = el.innerHTML;
+		el.innerHTML = "";
+		ei = i;
+		ej = j;
+		var q = true;
+		for(i = 0; i < 4; ++i)
+			for(j = 0; j < 4; ++j)
+				if(i + j != 6 && document.getElementById(i + " " + j).innerHTML != i*4 + j + 1){
+					q = false;
+					break;
+				}
+				if(q) alert("Victory!");
+			}
+}
+function newGame(){			
+	for(i = 0; i < 4; ++i){
+		arr[i] = []
+		for(j = 0; j < 4; ++j){
 			if(i + j != 6)
 				arr[i][j] = i*4 + j + 1;
 			else
 				arr[i][j] = "";
 		}
 	}
-	ei = 3;//Запоминаем индексы элемента массива,
-	ej = 3;// в котором записана пустая строка.
-for(i = 0; i &lt; 2012; ++i) 
-	switch(Math.round(3*Math.random())){ // Случайным образом выбираем число от 0 до 3				
-			case 0: if(ei != 0) swap(arr,ei,ej,--ei,ej); break; 
-			case 1: if(ej != 3) swap(arr,ei,ej,ei, ++ej); break; 
-			case 2: if(ei != 3) swap(arr,ei,ej,++ei,ej); break; 
-			case 3: if(ej != 0) swap(arr,ei,ej,ei,--ej); 
+	ei = 3;
+	ej = 3;
+	for(i = 0; i < 1600; ++i)
+		switch(Math.round(3*Math.random())){
+			case 0: if(ei != 0) swap(arr,ei,ej,--ei,ej); break; // up
+			case 1: if(ej != 3) swap(arr,ei,ej,ei, ++ej); break; // right
+			case 2: if(ei != 3) swap(arr,ei,ej,++ei,ej); break; // down
+			case 3: if(ej != 0) swap(arr,ei,ej,ei,--ej); // left
 		}
-var table = document.createElement("table"); //Cоздаём таблицу	
-for(i = 0; i &lt; 4; ++i){
-		var row = document.createElement("tr"); //Добавляем в неё строки
-		for(j = 0; j &lt; 4; ++j){
-			var cell = document.createElement("td");//Cоздаём ячейки
-			cell.id = i + " " + j; // Привязываем к событию cell click
-			cell.onclick = cellClick; //Записываем в ячейку соответсвующий эл-т массива
-			cell.innerHTML = arr[i][j];
-			row.appendChild(cell);// Добавляем ячейку в строку
+	var table = document.createElement("table"),
+		tbody = document.createElement("tbody");					
+	table.appendChild(tbody);
+	for(i = 0; i < 4; ++i){
+		var row = document.createElement("tr");
+		for(j = 0; j < 4; ++j){
+			var cell = document.createElement("td");
+				cell.id = i + " " + j;
+				cell.onclick = cellClick;
+				cell.innerHTML = arr[i][j];
+				row.appendChild(cell);
 		}
-	table.appendChild(row);// Добавляем строку в итаблицу			
-}
-if(box.childNodes.length == 1) //Проверка на состояние таблицы
-box.removeChild(box.firstChild); //Удаляем таблицу, если она есть	
-box.appendChild(table);// Запихиваем в box table</div>
-}
-function swap(arr,i1,j1,i2,j2){				
-		t = arr[i1][j1];
-		arr[i1][j1] = arr[i2][j2];
-		arr[i2][j2] = t;
-}
-
-function cellClick(e){				
-var el = e.srcElement || e.target;
-	//получаем номер строки и столбца, на пересечении которых расположена ячейка. 
-	var i = el.id.charAt(0),
-	j = el.id.charAt(2);
-		if((i == ei &amp;&amp; Math.abs(j - ej) == 1) || (j == ej &amp;&amp; Math.abs(i - ei) == 1)){
-		document.getElementById(ei + " " + ej).innerHTML = el.innerHTML;
-		el.innerHTML = "";//Запоминаем положение пустой ячейки
-			ei = i;
-			ej = j;
-	var q = true;
-	//Проверка комбинации ячеек
-	for(i = 0; i &lt; 4; ++i)
-		for(j = 0; j &lt; 4; ++j)
-			if(i + j != 6 &amp;&amp;
-			document.getElementById(i + " " + j).innerHTML != i*4 + j + 1){
-				q = false;
-				break;
-		}
-	if(q) alert("Victory!");
+		tbody.appendChild(row);					
 	}
-}
-window.onload = function() {
-		box = document.getElementById("box");
-			newGame();				
-			document.getElementById("reset").onclick = newGame;
+	if(box.childNodes.length == 1)
+		box.removeChild(box.firstChild);	
+	box.appendChild(table);	
 }
