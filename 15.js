59a77dc4842e74a8d69d41926c0657c02cf58b4c
Nikita <36260669+magus98@users.noreply.github.com>
2019-05-22 14:44:51 +0300

    Update 15.js

--- 15.js
+++ 15.js
@@ -1,48 +1,69 @@
 function newGame(){
-var arr = [];
-for(i = 0; i &lt; 4; ++i){
-		arr[i] = [];
-		for(j = 0; j &lt; 4; ++j){
-			if(i + j != 6)
-				arr[i][j] = i*4 + j + 1;
-			else
-				arr[i][j] = "";
+    var arr = [];
+    for(i = 0; i &lt; 4; ++i){
+        arr[i] = [];
+	for(j = 0; j &lt; 4; ++j){
+	    if(i + j != 6){
+	        arr[i][j] = i*4 + j + 1;
+	    }
+	    else{
+	        arr[i][j] = "";
+	    }
+	}
+    }
+    ei = 3;//Запоминаем индексы элемента массива,
+    ej = 3;// в котором записана пустая строка.
+    for(i = 0; i &lt; 2012; ++i){ 
+        switch(Math.round(3*Math.random())){ // Случайным образом выбираем число от 0 до 3				
+	    case 0:
+	        if(ei != 0){
+		    swap(arr,ei,ej,--ei,ej);
+		}
+		break; 
+	    case 1:
+		if(ej != 3){ 
+		    swap(arr,ei,ej,ei, ++ej);
+		}
+	        break; 
+	    case 2:
+		if(ei != 3){
+		    swap(arr,ei,ej,++ei,ej);
+		}
+		break; 
+	    case 3: 
+		if(ej != 0){
+		    swap(arr,ei,ej,ei,--ej); 
 		}
 	}
-	ei = 3;//Запоминаем индексы элемента массива,
-	ej = 3;// в котором записана пустая строка.
-for(i = 0; i &lt; 2012; ++i) 
-	switch(Math.round(3*Math.random())){ // Случайным образом выбираем число от 0 до 3				
-			case 0: if(ei != 0) swap(arr,ei,ej,--ei,ej); break; 
-			case 1: if(ej != 3) swap(arr,ei,ej,ei, ++ej); break; 
-			case 2: if(ei != 3) swap(arr,ei,ej,++ei,ej); break; 
-			case 3: if(ej != 0) swap(arr,ei,ej,ei,--ej); 
-		}
-var table = document.createElement("table"); //Cоздаём таблицу	
-for(i = 0; i &lt; 4; ++i){
-		var row = document.createElement("tr"); //Добавляем в неё строки
-		for(j = 0; j &lt; 4; ++j){
-			var cell = document.createElement("td");//Cоздаём ячейки
-			cell.id = i + " " + j; // Привязываем к событию cell click
-			cell.onclick = cellClick; //Записываем в ячейку соответсвующий эл-т массива
-			cell.innerHTML = arr[i][j];
-			row.appendChild(cell);// Добавляем ячейку в строку
-		}
+    }
+    var table = document.createElement("table"); //Cоздаём таблицу	
+    for(i = 0; i &lt; 4; ++i){
+        var row = document.createElement("tr"); //Добавляем в неё строки
+	for(j = 0; j &lt; 4; ++j){
+	    var cell = document.createElement("td");//Cоздаём ячейки
+	    cell.id = i + " " + j; // Привязываем к событию cell click
+	    cell.onclick = cellClick; //Записываем в ячейку соответсвующий эл-т массива
+	    cell.innerHTML = arr[i][j];
+	    row.appendChild(cell);// Добавляем ячейку в строку
+	}
 	table.appendChild(row);// Добавляем строку в итаблицу			
-}
-if(box.childNodes.length == 1) //Проверка на состояние таблицы
-box.removeChild(box.firstChild); //Удаляем таблицу, если она есть	
-box.appendChild(table);// Запихиваем в box table</div>
+    }
+    if(box.childNodes.length == 1){ //Проверка на состояние таблицы
+        box.removeChild(box.firstChild); //Удаляем таблицу, если она есть	
+        box.appendChild(table);// Запихиваем в box table</div>
+    }
 }
 function swap(arr,i1,j1,i2,j2){				
-		t = arr[i1][j1];
-		arr[i1][j1] = arr[i2][j2];
-		arr[i2][j2] = t;
+    t = arr[i1][j1];
+    arr[i1][j1] = arr[i2][j2];
+    arr[i2][j2] = t;
 }
 
-var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40; //Объявляем переменные клавиш
+    var LEFT = 37, UP = 38, RIGHT = 39, DOWN = 40; //Объявляем переменные клавиш
 
-window.onload = function () {  
-  document.body.onkeydown = function (e1){
-	   var e2 = e2 || window.event,
-	   keyCode = e1.keyCode;
+window.onload = function (){  
+    document.body.onkeydown = function (e1){
+        var e2 = e2 || window.event,
+	 keyCode = e1.keyCode;
+    }
+}
