6e6236edd73e94181f10e4d272a6271d75e4a7cb
Nikita <36260669+magus98@users.noreply.github.com>
2019-06-02 21:22:38 +0300

    Update 15.js

--- 15.js
+++ 15.js
@@ -4,9 +4,8 @@ function newGame(){
         arr[i] = [];
         for(j = 0; j &lt; 4; ++j){
             if(i + j != 6){
-	        arr[i][j] = i*4 + j + 1;
-	    } 
-	    else{
+	        arr[i][j] = i * 4 + j + 1;
+	    } else {
 	        arr[i][j] = "";
 	    }
 	}
